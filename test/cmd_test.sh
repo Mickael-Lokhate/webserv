ecd74b052202ea46fb9d9d3dcea808e166a1e5f5
El hocine Takouert <etakouer@c3r1p1.42nice.fr>
2021-11-13 19:03:16 +0100

    tester resume

---
+++ test/cmd_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# faites vos tests a partir goinfre svp
+
+echo $(python -c "print('A' * 100000)") > data && echo start && curl -X POST http://localhost/directory/youpi.bla --data-binary "@data" > out
+
+mkfifo piper
+nc -vlk 8000 < piper | tee -a in.http | nc localhost 80 | tee -a out.http > piper
+
+socat -r req.http -R res.http TCP-LISTEN:8000,fork,reuseaddr TCP:localhost:80
