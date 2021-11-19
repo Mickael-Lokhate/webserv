cbba8698e666ce222fe63fc9cef382db508372e4
mlokhate <mlokhate@c2r1p3.42nice.fr>
2021-11-19 13:43:25 +0100

    Update Loader.cpp to adapt with route and server

--- srcs/Loader.cpp
+++ srcs/Loader.cpp
@@ -20,25 +20,26 @@ Loader&	Loader::operator=(const Loader& rhs)
 	std::cout << "Assignation of a Loader" << std::endl;
 }
 
-void	Loader::add_servers(std::vector<Servers> *servers)
+void	Loader::add_servers(std::vector<Server> *servers)
 {
-	std::cout << "Adding a server..." << std::endl;
 	_parse_config(servers);
-	// Creer un nouvel objet Server
-	// Appel _parse_server pour initialiser le nouveau server
-	// Ajout du nouveau Server au vecteur
 }
 
-void	Loader::_parse_config(std::vector<Servers> *servers)
+void	Loader::_parse_config(std::vector<Server> *servers)
 {
 	std::cout << "Configuration parsing start" << std::endl;
 	// Parse le fichier de config
-	// Appel _add_server quand on rencontre un nouveau server
+	// Bloc server trouve
+	Server	new_server;
+	_parse_server(&new_server);
+	// Ajout du serveur au vecteur
 }
 
 void	Loader::_parse_server(Server *new_server)
 {
 	std::cout << "Parse  of a server start" << std::endl;
 	// Setup tous les attributs du server rencontre
-	// Appel fonction add_route du server si "Location" est trouve
+	t_route_conf	new_route_conf;
+	// Setup les attributs de new_route_conf avec les valeurs du fichier de conf ou NULL 
+	new_server->add_route(new_route_conf);
 }
