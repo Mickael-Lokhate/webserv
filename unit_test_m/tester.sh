7f0d2f28a2c443e52003c9d27cfa6878edd3713d
mlokhate <mlokhate@c2r1p3.42nice.fr>
2021-11-25 15:02:56 +0100

    Tester OK

--- unit_test_m/tester.sh
+++ unit_test_m/tester.sh
@@ -14,21 +14,24 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'
 
-echo -e "${Red}Compiling....${Reset}"
+echo -e "To redirect the output of each test add 1 argument\n"
+echo -e "${Red}Compiling....${Reset}\n"
 clang++ -Wall -Wextra -I$INC_PATH -g -fsanitize=address ${SRCS_PATH}Loader.cpp ${SRCS_PATH}Server.cpp ${SRCS_PATH}Route.cpp ${SRCS_PATH}LoaderTreatment.cpp ./main_test_loader.cpp
 
 i=1
 for f in $CONF_PATH
 do
 	echo -e "${Blue}Executing Test nº$i${Reset} - ${Red}FILE : $f${Reset}"
-	if $1
+	if [ $# -eq 1 ] 
+	then
 		./a.out $f > test_output/test_${i}.out
 	else
 		./a.out $f 
 	fi
 	((i=i+1))
 done
-if $1
-	echo -e "${Green}The output are redirect in files in test_output folder${Reset}"
+if [ $# -eq 1 ]
+then
+	echo -e "\n${Green}The output of each tests has been redirected in a file test_<test number>.out in the test_output folder${Reset}"
 fi
 rm -rf a.out a.out.dSYM
